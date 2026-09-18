9c084216001ccea7ab6aa1a6cc97fa9e376c2557
saldiviafabian07-cell <saldiviafabian07@gmail.com>
2026-09-17 21:43:46 -0300

    Delete js directory

--- js/friends.js
+++
@@ -1,9 +0,0 @@
-// Punto de integración de amigos, solicitudes e invitaciones.
-// La implementación de operaciones permanece en app para conservar sus rutas Firebase.
-export function createFriendsController({ state, renderAccountUI, renderLobbyFriends }) {
-  function refresh() {
-    renderAccountUI?.();
-    renderLobbyFriends?.();
-  }
-  return { state, refresh };
-}
