afee3d7f563b983d82edceed4a542311f4b93c7b
saldiviafabian07-cell <saldiviafabian07@gmail.com>
2026-09-17 21:10:26 -0300

    Add files via upload

---
+++ data/whoami/categories.js
@@ -0,0 +1,18 @@
+// Categorías editables para ¿Quién soy?
+export const categories = [
+  "Los Simpson",
+  "Malcolm in the Middle",
+  "Cantantes",
+  "Personajes de Dibujos Animados",
+  "Futbolistas",
+  "Películas",
+  "Personajes Chilenos 🇨🇱",
+  "Anime — Nivel Fácil",
+  "Youtubers / Streamers",
+  "Personajes de Videojuegos 🎮",
+  "Superhéroes 🦸",
+  "Villanos 😈",
+  "Otros / Random 🤡",
+  "Anime — Nivel Medio / Arriesgado 🍥"
+];
+
