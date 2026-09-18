7b852ef32e82bd4db5d48951bb75b33bf53d54a4
saldiviafabian07-cell <saldiviafabian07@gmail.com>
2026-09-17 21:08:57 -0300

    Delete data directory

--- data/age/data.js
+++
@@ -1,9 +0,0 @@
-// Parámetros editables de Adivina la Edad.
-// La lógica del juego sigue en js/games/age.js.
-export const ageData = {
-  minAge: 0,
-  maxAge: 10000,
-  defaultRounds: 3,
-  preparationSeconds: 10,
-  revealSeconds: 15
-};
